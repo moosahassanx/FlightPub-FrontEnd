ca33724dcd1a8c9b192215283f35509f12256b1e
mikeKinani <83221589+mikeKinani@users.noreply.github.com>
2021-08-22 18:26:41 +1000

    removing features page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,59 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import TravelBot from './TravelBot'
-import '../Css/footer.css'
-import { AiFillFacebook, AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
+import TravelBot from "./TravelBot";
+import "../Css/footer.css";
+import {
+	AiFillFacebook,
+	AiFillTwitterCircle,
+	AiFillInstagram,
+} from "react-icons/ai";
 import { GrFacebook } from "react-icons/gr";
 
-
 const Footer = () => {
+	// Local state until extending to redux
+	const [open, isOpen] = useState(false);
+	const onClick = () => isOpen(!open);
 
-    // Local state until extending to redux
-    const [open, isOpen] = useState(false);
-    const onClick = () => isOpen(!open);
-
-    return(
-        <div className="footer-main">
-            <div className="container">
-                <div className="row">
-                    {/* Col 1 */}
-                    <div className="col-md-4 col-md-offset-3">
-                        <h3>Contact us</h3>
-                        <ul className="list-unstyled">
-                            <li>Email: enquires@flighpub.com</li>
-                            <li>P: 2342 4234</li>
-                            <li>M: 0402 324 543</li>
-                        </ul>
-                    </div>
-                    {/* Col 2 */}
-                    <div className="col-md-4">
-                        <h2>Company</h2>
-                        <ul className="list-unstyled">
-                            <li>42 Wallaby way, Sydney </li>
-                        </ul>
-
-                    </div>
-                    {/* Col 3 */}
-                    <div className="list-unstyled">
-                        <h2>Follow us</h2>
-                        <GrFacebook /> Facebook<br/>
-                        <AiFillTwitterCircle /> Twitter<br/>
-                        <AiFillInstagram/>Instagram
-                    </div>
-
-                </div>
-                <div className="row">
-                    <p className="col-sm">
-                        &copy;{new Date().getFullYear()} FlightPub | All rights reserved | <a href="/">TOC</a> | <a href="/">Privacy statement</a>
-                    </p>
-                </div>
-            </div>
-            <div className="float-right">
-                            {open ? <TravelBot /> : null}
-                            <button onClick={onClick}>TravelBot</button>
-            </div>
-        </div>
-
-    )
-}
+	return (
+		<div className='footer-main'>
+			<div className='container'>
+				<div className='row'>
+					{/* Col 1 */}
+					<div className='col-md-4 col-md-offset-3'>
+						<h2>Contact us</h2>
+						<ul className='list-unstyled'>
+							<li>Email: enquires@flighpub.com</li>
+							<li>P: 2342 4234</li>
+							<li>M: 0402 324 543</li>
+						</ul>
+					</div>
+					{/* Col 2 */}
+					<div className='col-md-4'>
+						<h2>Company</h2>
+						<ul className='list-unstyled'>
+							<li>42 Wallaby way, Sydney </li>
+						</ul>
+					</div>
+					{/* Col 3 */}
+					<div className='list-unstyled'>
+						<h2>Follow us</h2>
+						<GrFacebook /> Facebook
+						<br />
+						<AiFillTwitterCircle /> Twitter
+						<br />
+						<AiFillInstagram />
+						Instagram
+					</div>
+				</div>
+				<div className='row'>
+					<p className='col-sm'>
+						&copy;{new Date().getFullYear()} FlightPub | All rights reserved |{" "}
+						<a href='/'>TOC</a> | <a href='/'>Privacy statement</a>
+					</p>
+				</div>
+			</div>
+			<div className='float-right'>
+				{open ? <TravelBot /> : null}
+				<button onClick={onClick}>TravelBot</button>
+			</div>
+		</div>
+	);
+};
 
 export default Footer;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,238 +1,245 @@
 // imports
-import {useState, useEffect} from 'react'
-import { Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../Images/FlightPub-Logo.png";
-import '../Css/app.css'
-import {useDispatch} from 'react-redux';
-import {login, username} from '../Actions';
-import RegisterForm from './RegisterForm';
-
+import "../Css/app.css";
+import { useDispatch } from "react-redux";
+import { login, username } from "../Actions";
+import RegisterForm from "./RegisterForm";
 
 const NavBar = () => {
-    // Calls the user API on Home page mount
-    // Return a JSON string of all users in the DB
-    // Only updates on Home component mount
-    useEffect(() => {
-        // local storage (refreshing the page wont make you lose login state data)
-        const loginStateData = localStorage.getItem('user-login-state');
-        const loginNameData = localStorage.getItem('user-login-name');
-        const loginLastNameData = localStorage.getItem('user-login-last-name');
-        const loginEmailData = localStorage.getItem('user-login-email');
-        const loginPasswordData = localStorage.getItem('user-login-password');
-        const loginTypeData = localStorage.getItem('user-login-type');
+	// Calls the user API on Home page mount
+	// Return a JSON string of all users in the DB
+	// Only updates on Home component mount
+	useEffect(() => {
+		// local storage (refreshing the page wont make you lose login state data)
+		const loginStateData = localStorage.getItem("user-login-state");
+		const loginNameData = localStorage.getItem("user-login-name");
+		const loginLastNameData = localStorage.getItem("user-login-last-name");
+		const loginEmailData = localStorage.getItem("user-login-email");
+		const loginPasswordData = localStorage.getItem("user-login-password");
+		const loginTypeData = localStorage.getItem("user-login-type");
 
-        if(loginStateData)      // if react picks up on previous data then use this data rather than generating entirely new page
-        {
-            setLoggedIn(JSON.parse(loginStateData));
-            setLoggerName(JSON.parse(loginNameData));
-            setLoggerLastName(JSON.parse(loginLastNameData));
-            setLoggerEmail(JSON.parse(loginEmailData));
-            setLoggerPassword(JSON.parse(loginPasswordData));
-            setLoggerType(JSON.parse(loginTypeData));
-        }
-    }, [])
+		if (loginStateData) {
+			// if react picks up on previous data then use this data rather than generating entirely new page
+			setLoggedIn(JSON.parse(loginStateData));
+			setLoggerName(JSON.parse(loginNameData));
+			setLoggerLastName(JSON.parse(loginLastNameData));
+			setLoggerEmail(JSON.parse(loginEmailData));
+			setLoggerPassword(JSON.parse(loginPasswordData));
+			setLoggerType(JSON.parse(loginTypeData));
+		}
+	}, []);
 
-    // redux
-    const dispatch = useDispatch();
+	// redux
+	const dispatch = useDispatch();
 
-    // const[state, function to update the state]
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [loggerName, setLoggerName] = useState("null");
-    const [loggerLastName, setLoggerLastName] = useState("null");
-    const [loggerEmail, setLoggerEmail] = useState("null");
-    const [LoggerPassword, setLoggerPassword] = useState("null");
-    const [loggerType, setLoggerType] = useState("null");
-    const [openRegistrationForm, setOpenRegistrationForm] = useState(false);
+	// const[state, function to update the state]
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [loggerName, setLoggerName] = useState("null");
+	const [loggerLastName, setLoggerLastName] = useState("null");
+	const [loggerEmail, setLoggerEmail] = useState("null");
+	const [LoggerPassword, setLoggerPassword] = useState("null");
+	const [loggerType, setLoggerType] = useState("null");
+	const [openRegistrationForm, setOpenRegistrationForm] = useState(false);
 
-    // login state use effects
-    useEffect(() => {
-        localStorage.setItem('user-login-state', JSON.stringify(loggedIn));
-        localStorage.setItem('user-login-name', JSON.stringify(loggerName));
-        localStorage.setItem('user-login-last-name', JSON.stringify(loggerLastName));
-        localStorage.setItem('user-login-email', JSON.stringify(loggerEmail));
-        localStorage.setItem('user-login-password', JSON.stringify(LoggerPassword));
-        localStorage.setItem('user-login-type', JSON.stringify(loggerType))
-    })
+	// login state use effects
+	useEffect(() => {
+		localStorage.setItem("user-login-state", JSON.stringify(loggedIn));
+		localStorage.setItem("user-login-name", JSON.stringify(loggerName));
+		localStorage.setItem(
+			"user-login-last-name",
+			JSON.stringify(loggerLastName)
+		);
+		localStorage.setItem("user-login-email", JSON.stringify(loggerEmail));
+		localStorage.setItem("user-login-password", JSON.stringify(LoggerPassword));
+		localStorage.setItem("user-login-type", JSON.stringify(loggerType));
+	});
 
-    // the actual business logic for a user logging in
-    function registrationForm()
-    {
-        setOpenRegistrationForm(true);
-    }
+	// the actual business logic for a user logging in
+	function registrationForm() {
+		setOpenRegistrationForm(true);
+	}
 
-    // Async API call to fetch the users in the db
-    async function loginChecker() {
-        // TODO: have users login with secure encryption..
-        
-        // TODO: this might be used later - if not, remove this in the final version
-        const loginCredentials = [{
-            userName: document.getElementById("username").value,
-            password: document.getElementById("password").value
-        }]
+	// Async API call to fetch the users in the db
+	async function loginChecker() {
+		// TODO: have users login with secure encryption..
 
-        var loginSuccessful = false;
+		// TODO: this might be used later - if not, remove this in the final version
+		const loginCredentials = [
+			{
+				userName: document.getElementById("username").value,
+				password: document.getElementById("password").value,
+			},
+		];
 
-        // GET request using fetch with error handling
-        await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                userName: document.getElementById("username").value,
-                password: document.getElementById("password").value
-            })
-        })
-        .then(async response => {
-            const data = await response.json();
+		var loginSuccessful = false;
 
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response statusText
-                const error = (data && data.message) || response.statusText;
-                return Promise.reject(error);
-            }            
+		// GET request using fetch with error handling
+		await fetch("/login", {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				userName: document.getElementById("username").value,
+				password: document.getElementById("password").value,
+			}),
+		})
+			.then(async (response) => {
+				const data = await response.json();
 
-            console.log("LOGIN SUCCESSFUL!!")
-            console.log(data.map((object => object)))
-            console.log("WELCOME " + data.map((object => object.userName)))
+				// check for error response
+				if (!response.ok) {
+					// get error message from body or default to response statusText
+					const error = (data && data.message) || response.statusText;
+					return Promise.reject(error);
+				}
 
-            // set status
-            loginSuccessful= true;
+				console.log("LOGIN SUCCESSFUL!!");
+				console.log(data.map((object) => object));
+				console.log("WELCOME " + data.map((object) => object.userName));
 
-            // local storage setting
-            var returnerFirstName = data.map(object => object.firstName)
-            var returnerLastName = data.map(object => object.lastName)
-            var returnerUserName = data.map(object => object.userName)
-            var returnerPasswordHash = data.map(object => object.passwordHash)
-            var returnerUserType = data.map(object => object.userType)
+				// set status
+				loginSuccessful = true;
 
-            setLoggerName(returnerFirstName)
-            setLoggerLastName(returnerLastName)
-            setLoggerEmail(returnerUserName)
-            setLoggerPassword(returnerPasswordHash)
-            setLoggerType(returnerUserType)
-        })
+				// local storage setting
+				var returnerFirstName = data.map((object) => object.firstName);
+				var returnerLastName = data.map((object) => object.lastName);
+				var returnerUserName = data.map((object) => object.userName);
+				var returnerPasswordHash = data.map((object) => object.passwordHash);
+				var returnerUserType = data.map((object) => object.userType);
 
-        // react catch
-        .catch(error => {
-            console.error('LOGIN ERROR: ', error.toString());
-        });
-        
-        // decision based on successful credentials
-        if(loginSuccessful == true)
-        {
-            setLoggedIn(true);
+				setLoggerName(returnerFirstName);
+				setLoggerLastName(returnerLastName);
+				setLoggerEmail(returnerUserName);
+				setLoggerPassword(returnerPasswordHash);
+				setLoggerType(returnerUserType);
+			})
 
-            // redux setting
-            dispatch(login());
-            dispatch(username());
-        }
+			// react catch
+			.catch((error) => {
+				console.error("LOGIN ERROR: ", error.toString());
+			});
 
-        // the fault is of the user - display error message
-        else
-        {
-            alert('Incorrect username or password.');
-        }
+		// decision based on successful credentials
+		if (loginSuccessful == true) {
+			setLoggedIn(true);
 
-    }
+			// redux setting
+			dispatch(login());
+			dispatch(username());
+		}
 
-    // users wants to log out (set all attributes back to default values)
-    function logOut()
-    {
-        setLoggedIn(false);
-        setLoggerName(null);
-        setLoggerLastName(null);
-        setLoggerEmail(null);
-        setLoggerPassword(null);
-        setLoggerType(null);
-        alert("Successfully logged out of Flightpub.");
-    }
+		// the fault is of the user - display error message
+		else {
+			alert("Incorrect username or password.");
+		}
+	}
 
+	// users wants to log out (set all attributes back to default values)
+	function logOut() {
+		setLoggedIn(false);
+		setLoggerName(null);
+		setLoggerLastName(null);
+		setLoggerEmail(null);
+		setLoggerPassword(null);
+		setLoggerType(null);
+		alert("Successfully logged out of Flightpub.");
+	}
 
-    // generating the html code depending on user login state
-    return (
-        <Navbar bg="dark" variant="dark">
-            {/* FlightPub Logo */}
-            <Navbar.Brand as={Link} to="/">
-                <img
-                src={logo}
-                width="50"
-                height="50"
-                alt="FlightPub logo"
-                />
-            </Navbar.Brand>
+	// generating the html code depending on user login state
+	return (
+		<Navbar bg='dark' variant='dark'>
+			{/* FlightPub Logo */}
+			<Navbar.Brand as={Link} to='/'>
+				<img src={logo} width='50' height='50' alt='FlightPub logo' />
+			</Navbar.Brand>
 
-            {/* links to other pages */}
-            <Nav className="mr-auto">
-                <Nav.Link href="\">Home</Nav.Link>
-                <Nav.Link href="\features">Features</Nav.Link>
-                <Nav.Link href="\Groups">Groups</Nav.Link>
-                <Nav.Link href="\discovery">Discovery</Nav.Link>
+			{/* links to other pages */}
+			<Nav className='mr-auto'>
+				<Nav.Link href='\'>Home</Nav.Link>
+				{/* <Nav.Link href="\features">Features</Nav.Link> */}
+				<Nav.Link href='\Groups'>Groups</Nav.Link>
+				<Nav.Link href='\discovery'>Discovery</Nav.Link>
 
-                {/* only appear when the user has logged in */}
-                {
-                    loggedIn == true ? (
-                        <Nav.Link href="\accountManagement">Account Management</Nav.Link>
-                    ) : null
-                }
-                {
-                    loggedIn == true ? (
-                        <Nav.Link href="\WishlistPage">Wishlist</Nav.Link>
-                    ) : null
-                }
+				{/* only appear when the user has logged in */}
+				{loggedIn == true ? (
+					<Nav.Link href='\accountManagement'>Account Management</Nav.Link>
+				) : null}
+				{loggedIn == true ? (
+					<Nav.Link href='\WishlistPage'>Wishlist</Nav.Link>
+				) : null}
 
-                {/* only appear if the user is admin */}
-                {
-                    loggerType == "admin" ? (
-                        <Nav.Link href="\adminControl">Admin Control</Nav.Link>
-                    ) : null
-                }
-            </Nav>
+				{/* only appear if the user is admin */}
+				{loggerType == "admin" ? (
+					<Nav.Link href='\adminControl'>Admin Control</Nav.Link>
+				) : null}
+			</Nav>
 
-            {
-                loggedIn === false ? (
-                    // user has not logged in
-                    <div className='login-redirect-wrapper'>
-                    <form>
-                        {/* input data from user */}
-                        <div className='login-wrapper-left'>
-                            <input className='form-input' type="text" id="username" name="username" placeholder='Username'/><br/>
-                            <input className='form-input' type="password" id="password" name="password" placeholder='Password'/>
-                        </div>
+			{loggedIn === false ? (
+				// user has not logged in
+				<div className='login-redirect-wrapper'>
+					<form>
+						{/* input data from user */}
+						<div className='login-wrapper-left'>
+							<input
+								className='form-input'
+								type='text'
+								id='username'
+								name='username'
+								placeholder='Username'
+							/>
+							<br />
+							<input
+								className='form-input'
+								type='password'
+								id='password'
+								name='password'
+								placeholder='Password'
+							/>
+						</div>
 
-                        {/* login and register buttons */}
-                        <div className='login-wrapper-right'>
-                            <submit className='form-button' type="button" onClick={loginChecker}>Log In</submit>
-                            <submit className='form-button' type="button" onClick={registrationForm}>Register</submit>
-                        </div>
-                        <div> {/* pop up registration form when user clicks on register button */}
-                            {openRegistrationForm == true ? (
-                                <RegisterForm/>
-                            ) : (
-                                null
-                            )}</div>
-                    </form>
-                </div>
-                ) : (
-                    // user has logged in
-                    <div className='login-redirect-wrapper'>
-                        {/* display login message and log out button */}
-                        <div className='login-wrapper-left'>
-                            <p className='nav-text'> Welcome {loggerName}.</p>
-                            <submit className='form-button' type='button' onClick={logOut}>Logout</submit>
-                        </div>
-                    </div>
-
-
-                )
-            }
-
-        </Navbar>
-
-    )
-}
+						{/* login and register buttons */}
+						<div className='login-wrapper-right'>
+							<submit
+								className='form-button'
+								type='button'
+								onClick={loginChecker}
+							>
+								Log In
+							</submit>
+							<submit
+								className='form-button'
+								type='button'
+								onClick={registrationForm}
+							>
+								Register
+							</submit>
+						</div>
+						<div>
+							{" "}
+							{/* pop up registration form when user clicks on register button */}
+							{openRegistrationForm == true ? <RegisterForm /> : null}
+						</div>
+					</form>
+				</div>
+			) : (
+				// user has logged in
+				<div className='login-redirect-wrapper'>
+					{/* display login message and log out button */}
+					<div className='login-wrapper-left'>
+						<p className='nav-text'> Welcome {loggerName}.</p>
+						<submit className='form-button' type='button' onClick={logOut}>
+							Logout
+						</submit>
+					</div>
+				</div>
+			)}
+		</Navbar>
+	);
+};
 
 export default NavBar;
