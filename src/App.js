c919adacc99ed4b51f9d40255a63b78e4aae3ee9
mikeKinani <83221589+mikeKinani@users.noreply.github.com>
2021-08-22 17:04:57 +1000

    made groups work and added some features to it

--- src/App.js
+++ src/App.js
@@ -1,49 +1,71 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { NavBar, Footer, CardCarousel} from './Components/index';
-import { Home, Features, Register, Business, AccountManagement, Discovery, Groups, BookingPage, TicketSelectionPage, PaymentPage, ConfirmPage, HolidayPackages, WishlistPage } from './Pages/index';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Css/app.css'
-import 'react-dates/lib/css/_datepicker.css';
-import 'react-dates/initialize';
-import AdminControl from './Pages/AdminControl';
-import ApplyForPosition from './Pages/ApplyForPosition';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { NavBar, Footer, CardCarousel } from "./Components/index";
+import {
+	Home,
+	Features,
+	Register,
+	Business,
+	AccountManagement,
+	Discovery,
+	Groups,
+	BookingPage,
+	TicketSelectionPage,
+	PaymentPage,
+	ConfirmPage,
+	HolidayPackages,
+	WishlistPage,
+} from "./Pages/index";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Css/app.css";
+import "react-dates/lib/css/_datepicker.css";
+import "react-dates/initialize";
+import AdminControl from "./Pages/AdminControl";
+import ApplyForPosition from "./Pages/ApplyForPosition";
 
- // Implementing React Router for the use of SPA and conditionally rendering pages
+// Implementing React Router for the use of SPA and conditionally rendering pages
 const Routing = () => {
-    return(
-        <div className="page-container">
-            <div className="content-wrapper">
-                <Router>
-                    <NavBar />
-                        <Switch>
-                            <Route exact path="/" component={Home} />
-                            <Route exact path="/features" component={Features} />
-                            <Route exact path="/Groups" component={Groups} />
-                            <Route exact path="/accountManagement" component={AccountManagement} />
-                            <Route exact path="/Discovery" component={Discovery} />
-                            <Route exact path="/HolidayPackages" component={HolidayPackages} />
-                            <Route exact path="/Booking" component={BookingPage} />
-                            <Route exact path="/TicketSelectionPage" component={TicketSelectionPage} />
-                            <Route exact path="/PaymentPage" component={PaymentPage} />
-                            <Route exact path="/ConfirmPage" component={ConfirmPage} />
-                            <Route exact path="/adminControl" component={AdminControl} />
-                            <Route exact path="/applyForPosition" component={ApplyForPosition} />
-                            <Route exact path="/WishlistPage" component={WishlistPage} />
-                        </Switch>
-                </Router>
-            </div>
-            <CardCarousel/>
-             <Footer />
-        </div>
-    )
-}
+	return (
+		<div className='page-container'>
+			<div className='content-wrapper'>
+				<Router>
+					<NavBar />
+					<Switch>
+						<Route exact path='/' component={Home} />
+						<Route exact path='/features' component={Features} />
+						<Route exact path='/Groups' component={Groups} />
+						<Route
+							exact
+							path='/accountManagement'
+							component={AccountManagement}
+						/>
+						<Route exact path='/Discovery' component={Discovery} />
+						<Route exact path='/HolidayPackages' component={HolidayPackages} />
+						<Route exact path='/Booking' component={BookingPage} />
+						<Route
+							exact
+							path='/TicketSelectionPage'
+							component={TicketSelectionPage}
+						/>
+						<Route exact path='/PaymentPage' component={PaymentPage} />
+						<Route exact path='/ConfirmPage' component={ConfirmPage} />
+						<Route exact path='/adminControl' component={AdminControl} />
+						<Route
+							exact
+							path='/applyForPosition'
+							component={ApplyForPosition}
+						/>
+						<Route exact path='/WishlistPage' component={WishlistPage} />
+					</Switch>
+				</Router>
+			</div>
+			{/* <CardCarousel/> */}
+			<Footer />
+		</div>
+	);
+};
 function App() {
-
-
-    return (
-        <Routing />
-    );
+	return <Routing />;
 }
 
 export default App;
--- src/Pages/Groups.js
+++ src/Pages/Groups.js
@@ -2,16 +2,19 @@ import "../Css/Groups.css";
 import { GoogleMap, Marker } from "@react-google-maps/api";
 import { ListGroup, Button, Form, ListGroupItem } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
 
 // MAP OPTIONS
-const center = {
-	lat: 22,
-	lng: 11,
-};
+// const center = {
+// 	lat: 22,
+// 	lng: 11,
+// };
 const containerStyle = {
 	width: "100%",
 	height: "100%",
 };
+
+const API_Key = "AIzaSyDE4QqTEwYOVTFexogb-Aj7KWkex3ize4A";
 
 const Groups = () => {
 	// Checking if the local storage has any users
@@ -19,13 +22,52 @@ const Groups = () => {
 	const users = JSON.parse(localStorage.getItem("user-group")) || [];
 	const localStorarageMarkers =
 		JSON.parse(localStorage.getItem("user-markers")) || [];
-
+	const loggedEmail =
+		JSON.parse(localStorage.getItem("user-login-email")) || null;
 	// Various states
 	const [selected, setSelected] = useState();
 	const [userInput, setUserInput] = useState("");
 	const [userGroup, setUserGroup] = useState(users);
 	const [markers, setMarkers] = useState(localStorarageMarkers);
-
+	const [locations, setLocations] = useState([]);
+	const [image, setImage] = useState();
+	const [selectedLocation, setSelectedLocation] = useState();
+	const [email, setEmail] = useState(loggedEmail);
+	const [center, setCenter] = useState({
+		lat: 22,
+		lng: 11,
+	});
+	async function getLocations(userName) {
+		var url = `/getUserHistory?userName=${userName}`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				let arr = data.concat(locations);
+				setLocations(arr);
+			});
+	}
+	useEffect(() => {
+		if (userGroup.length > 0) {
+			userGroup.forEach((user) => {
+				getLocations(user.username);
+			});
+		}
+	}, []);
+	useEffect(() => {
+		if (locations.length > 0) {
+			locations.forEach((location) => {
+				geocodeByAddress(
+					`${location.flight.destinationCode.airport}, ${location.flight.destinationCode.countryCode3.countryName}`
+				)
+					.then((results) => getLatLng(results[0]))
+					.then((res) => {
+						location.geoLoc = res;
+						setCenter(res);
+					});
+			});
+		}
+		setLocations(locations);
+	}, [locations]);
 	// Updates localstorage when ever a user is added or removed
 	useEffect(() => {
 		localStorage.setItem("user-group", JSON.stringify(userGroup));
@@ -51,7 +93,7 @@ const Groups = () => {
 			await fetch("/checkExists?username=" + userInput)
 				.then((res) => res.json())
 				.then((json) => setMarkers(json));
-
+			getLocations(userInput);
 			newUserGroup.push({
 				username: userInput,
 			});
@@ -63,7 +105,12 @@ const Groups = () => {
 	// Handles removing a selected user
 	const handleRemove = () => {
 		let newUserGroup = [...userGroup];
-
+		let arr = locations.filter((location) => {
+			if (location.user.userName !== selected) {
+				return location;
+			}
+		});
+		setLocations(arr);
 		let find = newUserGroup.find((x) => x.username === selected);
 		let index = newUserGroup.indexOf(find);
 
@@ -77,7 +124,33 @@ const Groups = () => {
 	const handleClick1 = (e) => {
 		setSelected(e);
 	};
-
+	async function getPlacePhotoRef(place) {
+		let url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${place}&inputtype=textquery&fields=photos&key=${API_Key}`;
+		return await fetch(url).then((response) => response.json());
+	}
+	async function getPlacePhoto(placeRef) {
+		let url = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&maxwidth=400&photo_reference=${placeRef}&key=${API_Key}`;
+		const imageURLQuery = await fetch(url)
+			.then((r) => r.blob())
+			.catch(console.error);
+		setImage(URL.createObjectURL(imageURLQuery));
+	}
+	const renderLocation = (location) => {
+		getPlacePhotoRef(location.airport)
+			.then((data) => {
+				getPlacePhoto(data);
+			})
+			.catch((error) => console.log(error));
+		setSelectedLocation(location);
+	};
+	async function handleWishlist() {
+		let url =
+			`/newWishlistItem?user_name=` +
+			email[0] +
+			`&countryCode3=` +
+			selectedLocation.destinationCode;
+		await fetch(url).then((response) => console.log(response));
+	}
 	return (
 		// Container holdering all page content
 		// 1 row and 2 cols
@@ -86,10 +159,18 @@ const Groups = () => {
 		// Currently correct lat and long for users are being stored
 		// Unable to map through and place user markers on map - Current makrer is a dummy marker
 		<div id='main-container-groups' className='container-fluid text-center'>
-			<h1>Main container</h1>
+			<h1>Groups</h1>
+			<h3>
+				To discover the locations visted by your friends, enter their user name
+				below
+			</h3>
+			<p>
+				Please note that your friend must agree to share their visted locations
+				in their account settings
+			</p>
 			<div id='content-row' className='row'>
 				<div id='list-col' className='col-md-4'>
-					<h2> Group List Container</h2>
+					<h2> Added users</h2>
 
 					<div id='list-group' className='row justify-content-center'>
 						<ListGroup variant='flush'>
@@ -124,20 +205,59 @@ const Groups = () => {
 								</Button>
 							</div>
 						</div>
+						<br />
+						<div className='map-col border'>
+							{selectedLocation && (
+								<>
+									{selectedLocation.airport},{" "}
+									{selectedLocation.countryCode3.countryName}
+									<br />
+									{image && <>{image}</>}
+									This destination is popular for: {selectedLocation.tags}
+									{email !== null && (
+										<>
+											<Button
+												variant='primary'
+												size='lg'
+												onClick={handleWishlist}
+												block
+											>
+												Add to wishlist
+											</Button>
+										</>
+									)}
+								</>
+							)}
+						</div>
 					</Form>
 				</div>
-
 				<div id='map-col' className='col-md-8'>
 					<GoogleMap
 						mapContainerStyle={containerStyle}
-						zoom={2}
+						zoom={1}
 						center={center}
-						key='AIzaSyDE4QqTEwYOVTFexogb-Aj7KWkex3ize4A'
+						key={API_Key}
 					>
-						<Marker
+						{locations.map((location, i) => {
+							return (
+								<>
+									{location.geoLoc && (
+										<Marker
+											key={API_Key}
+											position={location.geoLoc}
+											label={`${location.user.userName}`}
+											onClick={(event) =>
+												renderLocation(location.flight.destinationCode)
+											}
+										/>
+									)}
+								</>
+							);
+						})}
+						{/* <Marker
 							key='AIzaSyDE4QqTEwYOVTFexogb-Aj7KWkex3ize4A'
 							position={{ lat: 34, lng: 123 }}
-						/>
+						/> */}
 					</GoogleMap>
 				</div>
 			</div>
